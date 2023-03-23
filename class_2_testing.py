6da65fd39903992d3cf182a9938d4414a669989e
vasilveira <vitor.agrella.silveira@gmail.com>
2023-03-23 10:33:38 -0500

    Created Phyton file (2.1)
    I created the file exercise as requested

---
+++ class_2_testing.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+"Introduction"
+print("Hello, World")
+
+
+"Math simple operation"
+x = 10
+
+print("How much is 10 * 2?")
+print(x*2)
+
+print("End of today's class")
